5043738d5bd6cb7bf06b89bd1d393a28e9861909
camilosada12 <161945491+camilosada12@users.noreply.github.com>
2024-03-09 14:09:01 -0500

    Update correcionpython.py

--- 09-sesion-06-03/correcionpython.py
+++ 09-sesion-06-03/correcionpython.py
@@ -1,7 +1,7 @@
 
-# CORRECCIÓN PYTHON 
+CORRECCIÓN PYTHON 
 
-# 1. Tres personas deciden invertir su dinero para fundar una empresa. Cada una de ellas invierte una cantidad distinta. Obtener el porcentaje que cada quien invierte con respecto a la cantidad total invertida.
+ 1. Tres personas deciden invertir su dinero para fundar una empresa. Cada una de ellas invierte una cantidad distinta. Obtener el porcentaje que cada quien invierte con respecto a la cantidad total invertida.
 try:
         msgError = "Error, su inversión no es válida."
         msgOk = "Su Inversión fue registrada con éxito."
@@ -41,13 +41,13 @@ try:
 except:
         print(msgError)
 
-## 2. Un alumno desea saber cuál será su promedio general en las tres materias más difíciles que cursa y cuál será el promedio que obtendrá en cada una de ellas. Estas materias se evalúan como se muestra a continuación:
+ 2 Un alumno desea saber cuál será su promedio general en las tres materias más difíciles que cursa y cuál será el promedio que obtendrá en cada una de ellas. Estas materias se evalúan como se muestra a continuación:
 
-# La calificación de Matemáticas se obtiene de la siguiente manera: Examen 90% Promedio de tareas 10% En esta materia se pidió un total de tres tareas.
+ La calificación de Matemáticas se obtiene de la siguiente manera: Examen 90% Promedio de tareas 10% En esta materia se pidió un total de tres tareas.
 
-# La calificación de Física se obtiene de la siguiente manera: Examen 80% Promedio de tareas 20% En esta materia se pidió un total de dos tareas.
+ La calificación de Física se obtiene de la siguiente manera: Examen 80% Promedio de tareas 20% En esta materia se pidió un total de dos tareas.
 
-# La calificación de Química se obtiene de la siguiente manera: Examen 85% Promedio de tareas 15% En esta materia se pidió un promedio de tres tareas.
+ La calificación de Química se obtiene de la siguiente manera: Examen 85% Promedio de tareas 15% En esta materia se pidió un promedio de tres tareas.
 
 try:
         msgError = "Error, dato no válida."
@@ -138,7 +138,7 @@ except:
         print(msgError)
 
 
-## 3. Leer un real e imprimir si el número es negativo o positivo
+ 3 Leer un real e imprimir si el número es negativo o positivo
 try:
         msgError = "Error, su número no es válido."
         msgOk = "Su número fue registrada con éxito."
@@ -157,7 +157,7 @@ except:
         print(msgError)
 
 
-## 4. Leer un real e imprimir si el número es mayor a 200 o no.
+ 4 Leer un real e imprimir si el número es mayor a 200 o no.
 try:
         msgError = "Error, su número no es válido."
         msgOk = "Su número fue registrada con éxito."
@@ -177,7 +177,7 @@ except:
         print(msgError)
 
 
-## 5. Leer un real e imprimir si el número está en el rango de 50 y 100.
+ 5 Leer un real e imprimir si el número está en el rango de 50 y 100.
 try:
         msgError = "Error, su número no es válido."
         msgOk = "Su número fue registrada con éxito."
